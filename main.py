a8adfcdf4306733f9d320c8029da5f0179271ae6
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2024-12-03 22:23:40 -0500

    prog: cleaned real estate CSV

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pandas as pd
+import os
+
+def clean_data(input_file_path: str, output_file_path: str):
+    df = pd.read_csv(input_file_path, low_memory=False)
+    commercial = df.loc[df['Property Type'] == 'Commercial'][['List Year', 'Town', 'Assessed Value', 'Sale Amount']]
+    commercial = commercial.dropna()
+    commercial.to_csv(output_file_path, index=False)
+    print(commercial.count())
+
 def main():
-    pass
+    input_csv = './data/Real_Estate_Sales_2001-2021_GL.csv'
+    output_csv = './data/Real Estate.csv'
+    if os.path.exists(input_csv):
+        clean_data(output_csv)
 
 if __name__ == '__main__':
     main()
